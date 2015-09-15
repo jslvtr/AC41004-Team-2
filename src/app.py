f455a43074d473f2c4f6b1b32e71b4e8d31d6ac2
jslvtr <jslvtr@gmail.com>
2015-09-15 15:26:09 +0100

    Added session support; the thing to do later on is when a user logs in, add an auth token (and possibly user ID) to the session data.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,21 @@
 __author__ = 'jslvtr'
 
-from flask import Flask
+from flask import Flask, session, jsonify
+from src.common.sessions import MongoSessionInterface
+
 app = Flask(__name__)
+app.session_interface = MongoSessionInterface(db="enterprisegym")
+app.secret_key = "ajfjfBafbaf1565~/?"
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    if 'tmp' in session.keys():
+        return jsonify({"message": session['tmp']}), 200
+    else:
+        session['tmp'] = "You are logged in!"
+        return jsonify({"message": "empty"}), 200
 
-if __name__ == "__main__":
-    app.run()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
