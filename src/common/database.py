74f1dab97c0b3f70b4bf4c354da2cbfda74ca213
stamas02 <stamas01@gmail.com>
2015-09-17 11:44:30 +0100

    updated database

--- src/common/database.py
+++ src/common/database.py
@@ -5,16 +5,20 @@ __author__ = 'jamiekerr'
 
 
 class Database(object):
-    def __init__(self, user, password, url, port, database):
+
+    DATABASE = None
+
+    @staticmethod
+    def initialize(user, password, url, port, database):
         client = pymongo.MongoClient(host=url,
                                      port=port)
-        self.database = client[database]
-        self.database.authenticate(user, password)
+        Database.DATABASE = client[database]
+        Database.DATABASE.authenticate(user, password)
 
     @staticmethod
     def find(collection, query):
         if collection is not None:
-            return collection.find(query)
+            return Database.DATABASE[collection].find(query)
         else:
             raise pymongo.errors.InvalidOperation
 
@@ -28,20 +32,20 @@ class Database(object):
     @staticmethod
     def insert(collection, data):
         if collection is not None:
-            return collection.insert(data)
+            return Database.DATABASE[collection].insert(data)
         else:
             raise pymongo.errors.InvalidOperation
 
     @staticmethod
     def update(collection, query, data):
         if collection is not None:
-            return collection.update(query, data)
+            return Database.DATABASE[collection].update(query, data)
         else:
             raise pymongo.errors.InvalidOperation
 
     @staticmethod
     def remove(collection, query):
         if collection is not None:
-            return collection.remove(query)
+            return Database.DATABASE[collection].remove(query)
         else:
             raise pymongo.errors.InvalidOperation
