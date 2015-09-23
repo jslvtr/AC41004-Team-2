cda0fc82e341963b2bdbeb2742e62b36bd2d3211
jslvtr <jslvtr@gmail.com>
2015-09-23 13:24:37 +0100

    Quick hotfix to event tests when running in parallel in travis.

--- tests/unit/test_event_registration.py
+++ tests/unit/test_event_registration.py
@@ -16,33 +16,32 @@ class TestEventRegister(TestCase):
 
     def test_event_register(self):
         user = "Jamie"
-        event_id = "123456"
-        Event(title="Test event",
+        event = Event(title="Test event",
               description="Test description",
               start=datetime.datetime.utcnow().strftime('%m/%d/%Y %I:%M %p'),
-              end=(datetime.datetime.utcnow() + datetime.timedelta(hours=1)).strftime('%m/%d/%Y %I:%M %p'),
-              id_="123456").save_to_db()
+              end=(datetime.datetime.utcnow() + datetime.timedelta(hours=1)).strftime('%m/%d/%Y %I:%M %p'))
+        event.save_to_db()
 
-        EventRegister.register_for_event(user, event_id)
+        EventRegister.register_for_event(user, event.get_id())
 
-        self.assertIsNotNone(EventRegister.check_if_registered(user, event_id))
+        self.assertIsNotNone(EventRegister.check_if_registered(user, event.get_id()))
 
-        Database.remove("registrations", {"user": user, "event": event_id})
-        Database.remove(Event.COLLECTION, {'_id': event_id})
+        Database.remove("registrations", {"user": user, "event": event.get_id()})
+        Database.remove(Event.COLLECTION, {'_id': event.get_id()})
 
     def test_event_unregister(self):
         user = "Jamie"
-        event_id = "123456"
-        Event(title="Test event",
+        event = Event(title="Test event",
               description="Test description",
               start=datetime.datetime.utcnow().strftime('%m/%d/%Y %I:%M %p'),
               end=(datetime.datetime.utcnow() + datetime.timedelta(hours=1)).strftime('%m/%d/%Y %I:%M %p'),
-              id_="123456").save_to_db()
+              id_="123456")
+        event.save_to_db()
 
-        EventRegister.register_for_event(user, event_id)
+        EventRegister.register_for_event(user, event.get_id())
 
-        EventRegister.unregister_for_event(user, event_id)
+        EventRegister.unregister_for_event(user, event.get_id())
 
-        self.assertIsNone(EventRegister.check_if_registered(user, event_id))
+        self.assertIsNone(EventRegister.check_if_registered(user, event.get_id()))
 
-        Database.remove(Event.COLLECTION, {'_id': event_id})
+        Database.remove(Event.COLLECTION, {'_id': event.get_id()})
